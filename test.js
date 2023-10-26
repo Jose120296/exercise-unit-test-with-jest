8ea3c9cf8e91b3ab900659535a964fe865ed4d9c
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-10-26 00:03:00 +0000

    Actualización

--- test.js
+++ test.js
@@ -1,34 +1,48 @@
 const oneEuroIs = {
-    "JPY": 156.5, // yen japonés
-    "USD": 1.07, // dólar estadounidense
-    "GBP": 0.87, // libra esterlina
-  };
-  
-  // hay que definir estas funciones antes de usarlas en los tests
-  const fromDollarToYen = function(amountInDollars) {
-    const rate = oneEuroIs["JPY"] / oneEuroIs["USD"];
-    const amountInYen = amountInDollars * rate;
-    return amountInYen;
-  };
-  
-  const fromYenToPound = function(amountInYen) {
-    const rate = oneEuroIs["GBP"] / oneEuroIs["JPY"];
-    const amountInPound = amountInYen * rate;
-    return amountInPound;
-  };
-  
-  // hay que importar estas funciones desde el archivo donde se definen
-  const { fromEuroToDollar, sum } = require("./app.js");
-  
-  test("Converting from dollar to yen", function() {
-    const dollars = 10;
-    const expectedYen = dollars * (oneEuroIs["JPY"] / oneEuroIs["USD"]);
-    expect(fromDollarToYen(dollars)).toBe(expectedYen);
-  });
-  
-  test("Converting from yen to pound", function() {
-    const yen = 10000;
-    const expectedPound = yen * (oneEuroIs["GBP"] / oneEuroIs["JPY"]);
-    expect(fromYenToPound(yen)).toBe(expectedPound);
-  }); // faltaba un punto y coma
-  
+  "JPY": 156.5, // yen japonés
+  "USD": 1.07, // dólar estadounidense
+  "GBP": 0.87, // libra esterlina
+};
+
+
+const fromEuroToDollar = function(amountInEuro) {
+  const rate = oneEuroIs["USD"];
+  const amountInDollar = amountInEuro * rate;
+  return amountInDollar;
+};
+
+const fromDollarToYen = function(amountInDollar) {
+  const rate = oneEuroIs["JPY"] / oneEuroIs["USD"];
+  const amountInYen = amountInDollar * rate;
+  return amountInYen;
+};
+
+const fromYenToPound = function(amountInYen) {
+  const rate = oneEuroIs["GBP"] / oneEuroIs["JPY"];
+  const amountInPound = amountInYen * rate;
+  return amountInPound;
+};
+
+// exportamos las funciones para usarlas en otros archivos 
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
+// aquí podemos escribir los tests
+const { sum } = require("./app.js");
+
+test("Converting from euro to dollar", function() {
+  const euros = 3.5;
+  const expectedDollars = euros * oneEuroIs["USD"];
+  expect(fromEuroToDollar(euros)).toBe(expectedDollars);
+});
+
+test("Converting from dollar to yen", function() {
+  const dollars = 10;
+  const expectedYen = dollars * (oneEuroIs["JPY"] / oneEuroIs["USD"]);
+  expect(fromDollarToYen(dollars)).toBe(expectedYen);
+});
+
+test("Converting from yen to pound", function() {
+  const yen = 10000;
+  const expectedPound = yen * (oneEuroIs["GBP"] / oneEuroIs["JPY"]);
+  expect(fromYenToPound(yen)).toBe(expectedPound);
+});
